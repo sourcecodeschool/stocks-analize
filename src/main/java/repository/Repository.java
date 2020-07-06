41de8a58c00f86b74006ad3e84edf96bf67136d8
mrBrown <buzunov66@yandex.ru>
2020-07-06 20:43:22 +0500

    Info

---
+++ src/main/java/repository/Repository.java
@@ -0,0 +1,25 @@
+package repository;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Repository {
+
+    static final String pathPrefix = System.getenv("user.dir") + "/db" ;
+    static final String pathTo = "bag.dat";
+
+    //CRUD create update delete get list
+
+    public List<String> getBagsList() {
+        //read file, List<Bag> return;
+        return new ArrayList<>();
+    }
+
+    //Использовать файловую сериализацию
+
+    //ПортфельРепозиторий
+    //АктивнаяЕдиницаРекпозиторий
+    //Файл и путь к нему
+
+}
