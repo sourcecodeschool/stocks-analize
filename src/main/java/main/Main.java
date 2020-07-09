2f4a826be7e857fe990ff3875b3f3cefbf0f9d35
mrBrown <buzunov66@yandex.ru>
2020-07-09 19:35:30 +0500

    Change controller name

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    static Controller controller;
+    static Controller contr;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
