da6a25e951c2afdf6b9138e80e130bb133359f9a
@Aleks-glitch <dark.aleks1987@gmail.com>
2020-07-20 18:52:13 +0500

    Rename bag to portfolio Rename bags to portfolioList Add serialize method

--- src/main/java/repository/PortfolioRepositoryImpl.java
+++ src/main/java/repository/PortfolioRepositoryImpl.java
@@ -1,13 +1,15 @@
 package repository;
 
-import model.Bags;
+import model.Portfolio;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 public class PortfolioRepositoryImpl implements PortfolioRepository {
 
     private static PortfolioRepository repository;
+    static final String pathPrefix = System.getProperty("user.home") + "/db" ;
+    static final String pathTo = "Portfolio.dat";
 
     public static PortfolioRepository getInstance() {
         if (repository == null) {
@@ -20,30 +22,46 @@ public class PortfolioRepositoryImpl implements PortfolioRepository {
     }
 
     @Override
-    public void create(Bags bag) throws IOException {
-        EntityManager.bags.add(bag);
+    public void create(Portfolio portfolio) throws IOException {
+        EntityManager.portfolioList.add(portfolio);
         //serialize this EntityManager.bags
     }
 
     @Override
-    public Bags get(Long id) throws IOException, ClassNotFoundException {
-        return null;//EntityManager.bags.stream().filter()....;
+    public Portfolio get(Long portfolioId) throws IOException, ClassNotFoundException {
+
+return EntityManager.portfolioList.get(Math.toIntExact(portfolioId));
     }
 
     @Override
-    public void update(Bags bag) throws IOException {
-        EntityManager.bags.remove(bag);
-        EntityManager.bags.add(bag);
-        //serialize this EntityManager.bags
-        serializeList(EntityManager.bags);
+    public void update(Portfolio portfolio) throws IOException {
+        EntityManager.portfolioList.remove(portfolio);
+        EntityManager.portfolioList.add(portfolio);
+        //serialize this EntityManager.portfolioList
+        serializeList(EntityManager.portfolioList);
     }
 
     @Override
-    public void delete(Bags bags) throws IOException {
+    public void delete(Portfolio portfolio) throws IOException {
 
     }
 
-    private void serializeList(List<Bags> bagsList) {
+    private void serializeList(List<Portfolio> portfolioList) throws IOException {
+        File f = new File(pathPrefix+pathTo);
+        if(f.exists() && !f.isDirectory()) {                  //Проверка на наличие файла
+            File file = new File(pathPrefix+pathTo);
+        }
+        FileOutputStream fileOutputStream = new FileOutputStream(pathPrefix + pathTo);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(portfolioList);
+        objectOutputStream.close();
     }
 
+    public List<Portfolio> deSerializeList() throws IOException, ClassNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream(pathPrefix + pathTo);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        List<Portfolio> portfolioList = (List<Portfolio>) objectInputStream.readObject();
+        objectInputStream.close();
+        return portfolioList;
+    }
 }
