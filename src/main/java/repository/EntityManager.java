eb251317b383f2ece8a2d6b33c3d32fa752083d1
@Aleks-glitch <dark.aleks1987@gmail.com>
2020-07-23 19:44:14 +0500

    Написал метод get

--- src/main/java/repository/EntityManager.java
+++ src/main/java/repository/EntityManager.java
@@ -1,13 +1,10 @@
 package repository;
+
 import model.Portfolio;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class EntityManager implements Serializable {
-
-
     private static EntityManager entityManager;
     public static EntityManager getInstance() {
         if (entityManager == null) {
@@ -17,44 +14,31 @@ public class EntityManager implements Serializable {
     }
 
     private EntityManager() {
+        portfolioRepository = PortfolioRepositoryImpl.getInstance();
     }
-
-
 
     //список портфелей
-    public static List<Portfolio> portfolioList = new ArrayList<>();
-    static PortfolioRepository portfolioRepository = PortfolioRepositoryImpl.getInstance();
+    static PortfolioRepository portfolioRepository;
 
-    //...
-    public void GetBagsList() {
-
-// Попытка работать с файлом данных как с текстовым файлом
-//    Bags.Bags bagList[] = new Bags.Bags[3];{
-//        try {
-//            InputStream is = new FileInputStream(Bags.Bags.pathPrefix + Bags.Bags.pathTo);
-//            BufferedInputStream bis = new BufferedInputStream(is, 100_000);
-//            ObjectInputStream ois = new ObjectInputStream(bis);
-//            for (int i = 0; i < 3; i++) {
-//                for (int j = 0; j < 4; j++) {
-//                    String line = Files.readAllLines(Paths.get(Bags.Bags.pathPrefix + Bags.Bags.pathTo)).get(j);
-//                    Files.readAllLines(Paths.get(Bags.Bags.pathPrefix + Bags.Bags.pathTo)).get(j);
-//                }
-//                bagList[i] = (Bags.Bags) ois.readObject();
-//                System.out.println(bagList[i].getPortfolioName().toString());
-//            };}
-//        catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }}}
-//список активов
-//нужен для сохранения в репозиторий
+    void createPortfolio(Portfolio portfolio) {
+        try {
+            portfolioRepository.create(portfolio);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-    public void updateBag(Portfolio portfolio) throws IOException {
-        portfolioRepository.update(portfolio);
+    Portfolio get (Long portfolioId){
+        Portfolio portfolio = null;
+        try {
+            portfolio = portfolioRepository.get(portfolioId);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return portfolio;
     }
+//    void update(Portfolio portfolio) throws IOException;
+//    void delete(Portfolio portfolio) throws IOException;
 
 }
